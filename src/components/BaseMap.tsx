8411be8a56c049fe6e40d4917debaa87fc7b63ad
larsmoen <l-moen3@online.no>
2024-01-02 10:49:22 +0100

    Back on track

---
+++ src/components/BaseMap.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import { useMap } from "../hook/useMap";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibW9lbGFyIiwiYSI6ImNsbjFyMG9pdjAxajQyam11b3hwaTdodTQifQ.Y9pM1qwtedaEDOkiCx7QgQ";
+
+export const BaseMap = () => {
+  const mapRef = useRef<HTMLDivElement>(null);
+  useMap(mapRef);
+
+  return <div ref={mapRef} className="map" />;
+};
+
+export default BaseMap;
---
+++ src/hook/useMap.ts
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+import { Map } from 'mapbox-gl';
+import { initMap } from '../utils/initMap';
+
+export const useMap = (container: React.RefObject<HTMLDivElement>) => {
+
+    const mapInitRef = useRef<Map | null>(null);
+
+    useEffect(() => {
+        if (container.current) {
+
+            mapInitRef.current = initMap(
+                container.current,
+                [10.4, 63.42]
+            );
+
+        }
+    }, []);
+}
